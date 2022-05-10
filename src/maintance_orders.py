2de99e358cd0d6b16aa579d879657c54b6c0bcd1
Anbraten <anton@ju60.de>
2022-05-10 17:38:54 +0200

    add database and create maintenance orders

--- src/maintance_orders.py
+++ src/maintance_orders.py
@@ -1,9 +1,28 @@
-def create_maintance_order(carrier_id: str, assumption: str) -> None:
-    print("create_maintance_order")
-    print(carrier_id)
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
 
-def start_webserver_for_maintance_orders_ui():
-    print("start_webserver_for_maintance_orders_ui")
+# Select your transport with a defined url endpoint
+transport = AIOHTTPTransport(url="http://localhost:8123/v1/graphql", headers={'x-hasura-admin-secret': 'myadminsecretkey'})
 
-# start api and ui server after loading module
-start_webserver_for_maintance_orders_ui()
+# Create a GraphQL client using the defined transport
+client = Client(transport=transport, fetch_schema_from_transport=True)
+
+def create_maintenance_order(carrier_id: str, assumption: str) -> None:
+    query = gql(
+    """
+    mutation MyQuery($carrier_id: String!, $assumption: String!) {
+        insert_maintenance_orders_one(object: {carrier_id: $carrier_id, assumption: $assumption}) {
+            id
+            carrier_id
+            assumption
+        }
+    }
+    """
+    )
+
+    params = {
+        "carrier_id": carrier_id,
+        "assumption": assumption
+    }
+    result = client.execute(query, variable_values=params)
+    print(result)
