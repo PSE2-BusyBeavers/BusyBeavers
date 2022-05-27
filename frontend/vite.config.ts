b6da5be216bd132a2e96a49e357a97a9740bc183
Anbraten <anton@ju60.de>
2022-05-27 13:55:48 +0200

    adjust vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,29 @@
-import { defineConfig } from 'vite'
-import alias from '@rollup/plugin-alias'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import alias from "@rollup/plugin-alias";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 const projectRootDir = resolve(__dirname);
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  server: {
+    host: "0.0.0.0",
+    proxy: {
+      "/v1": "http://localhost:8124/",
+    },
+    hmr: {
+      clientPort: process.env.GITPOD_HOST ? 443 : undefined,
+    },
+  },
+  plugins: [
+    react(),
     alias({
       entries: [
         {
-          find: '@src',
-          replacement: resolve(projectRootDir, 'src')
-        }
-      ]
-    })]
-})
+          find: "@src",
+          replacement: resolve(projectRootDir, "src"),
+        },
+      ],
+    }),
+  ],
+});
