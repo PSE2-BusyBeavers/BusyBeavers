1bc47e81719e429c8c633eef8e58618a698ef7ae
Anbraten <anton@ju60.de>
2022-06-14 19:13:07 +0200

    add windi

--- frontend/ligenium-report/vite.config.ts
+++ frontend/ligenium-report/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from "vite";
-import alias from "@rollup/plugin-alias";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import alias from '@rollup/plugin-alias';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import WindiCSS from 'vite-plugin-windicss';
 
 const projectRootDir = resolve(__dirname);
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     proxy: {
-      "/v1": "http://localhost:8124/",
+      '/v1': 'http://localhost:8124/',
     },
     hmr: {
       clientPort: process.env.GITPOD_HOST ? 443 : undefined,
@@ -17,11 +18,12 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    WindiCSS(),
     alias({
       entries: [
         {
-          find: "@src",
-          replacement: resolve(projectRootDir, "src"),
+          find: '@src',
+          replacement: resolve(projectRootDir, 'src'),
         },
       ],
     }),
