e041307b9e05c2c0eecebe5720d9e5f4651c7122
Anbraten <anton@ju60.de>
2022-06-28 08:23:37 +0200

    imporve sensor page

--- frontend/ligenium-report/src/pages/Sensor.tsx
+++ frontend/ligenium-report/src/pages/Sensor.tsx
@@ -3,10 +3,9 @@ import { useEffect, useState } from 'react';
 import { useCreateCarrierDataEntryMutation } from '@src/api/client';
 import { debounce } from '@mui/material';
 
-const carrierId = 8;
-
 const Sensor = () => {
   const [recording, setRecording] = useState(false);
+  const [carrierId, setCarrierId] = useState(0);
 
   const [{ xAcceleration, yAcceleration }, setMotion] = useState({
     xAcceleration: 0,
@@ -16,6 +15,10 @@ const Sensor = () => {
   const [_, createCarrierDataEntry] = useCreateCarrierDataEntryMutation();
 
   const uploadData = debounce(() => {
+    if (!carrierId) {
+      return;
+    }
+
     createCarrierDataEntry({
       carrierId,
       type: 'acceleration',
@@ -30,43 +33,58 @@ const Sensor = () => {
     });
   }, 1000);
 
+  const handleMotionEvent = (event: DeviceMotionEvent) => {
+    if (!recording || !carrierId) {
+      return;
+    }
+
+    const _xAcceleration = event?.acceleration?.x || event?.accelerationIncludingGravity?.x;
+    const _yAcceleration = event?.acceleration?.y || event?.accelerationIncludingGravity?.y;
+
+    if (!_xAcceleration || !_yAcceleration) {
+      alert('No motion detected. Maybe your browser is blocking the sensor or you do not have such a sensor.');
+      return;
+    }
+
+    setMotion({
+      xAcceleration: _xAcceleration,
+      yAcceleration: _yAcceleration,
+    });
+
+    uploadData();
+  };
+
   useEffect(() => {
-    const handleMotionEvent = (event: any) => {
-      if (!recording) {
-        return;
-      }
-
-      if (
-        !event?.accelerationIncludingGravity?.x ||
-        !event?.acceleration?.x ||
-        !event?.accelerationIncludingGravity?.y ||
-        !event?.acceleration?.y
-      ) {
-        return;
-      }
-
-      setMotion({
-        xAcceleration: event.accelerationIncludingGravity?.x || event.acceleration?.x,
-        yAcceleration: event.accelerationIncludingGravity?.y || event.acceleration?.y,
-      });
-
-      uploadData();
-    };
-
     window.addEventListener('devicemotion', handleMotionEvent, true);
     return () => window.removeEventListener('devicemotion', handleMotionEvent);
   }, []);
 
   return recording ? (
     <>
-      <span>We are tracking your acceleration data now!</span>
+      <span>We are tracking your acceleration for carrier {carrierId} data now!</span>
       <p>
         {xAcceleration}:{yAcceleration}
       </p>
-      <button onClick={() => setRecording(false)}>stop recording</button>
+      <button
+        onClick={() => {
+          setCarrierId(0);
+          setRecording(false);
+        }}
+      >
+        stop recording
+      </button>
     </>
   ) : (
-    <button onClick={() => setRecording(true)}>start recording</button>
+    <>
+      <button onClick={() => carrierId && setRecording(true)}>start recording</button>
+      <span>CarrierID</span>
+      <input
+        type="text"
+        placeholder="Carrier Id"
+        value={carrierId}
+        onChange={(event) => setCarrierId(event.target.value ? parseInt(event.target.value) : 0)}
+      />
+    </>
   );
 };
 
